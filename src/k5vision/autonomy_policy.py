07eabc23d09955ae63837961e06e7dfd7d6bf53e
mkurtgerald <kurt.gerald@gmail.com>
2026-09-21 09:29:37 -0400

    Fix autonomy policy lint import

--- src/k5vision/autonomy_policy.py
+++ src/k5vision/autonomy_policy.py
@@ -3,7 +3,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from enum import StrEnum
-from typing import Iterable
+from collections.abc import Iterable
 
 from .neural_contracts import ActionProposal, AutonomyMode, ExecutionGrant
 
